82a473fbac9ff50f5b3024e0e1bc420c7fb782a3
Francoise <francoisenyiraa@gmail.com>
2022-10-28 09:43:56 +0200

    Online Shopping

---
+++ src/components/Cards.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../Styles/Cards.css';
+
+const Cards = ({item, handleClick}) => {
+    const {title, Manufacturer, price, img} = item;
+
+  return (
+    <div className='cards'>
+        <div className='image_box'>
+            <img src={img} alt="Image" />
+        </div>
+        <div className='details'>
+            <p>{title}</p>
+            <p>{Manufacturer}</p>
+            <p>Price - {price}Rwf</p>
+            <button onClick={()=>handleClick(item)}>Add to Cart</button>
+        </div>
+    </div>
+  )
+}
+
+export default Cards
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../Styles/Footer.css'
+
+const Footer = () => (
+  <div className="footer">
+    <a href="#">Contacts</a>
+    <a href="#">AboutUS</a>
+    <a href="#">Our Services</a>
+    <p><i>&#169; ShopX</i></p>
+  </div>
+);
+
+export default Footer;
